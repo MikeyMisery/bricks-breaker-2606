167af38467e950cc7b8f7bda0f62a55ffd8e8865
Mike Williams <mikewilliamscgd@gmail.com>
2026-06-01 11:53:21 -0400

    TODIO #2 & #3 - add bricks to vector & update render to render all bricks

--- Game.cpp
+++ Game.cpp
@@ -20,12 +20,18 @@ void Game::Reset()
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
-	brick.width = 10;
-	brick.height = 2;
-	brick.x_position = 0;
-	brick.y_position = 5;
-	brick.doubleThick = true;
-	brick.color = ConsoleColor::DarkGreen;
+	for (int i = 0; i < 5; ++i)
+	{
+		Box brick;
+		brick.width = 10;
+		brick.height = 2;
+		brick.x_position = i * 12;
+		brick.y_position = 5;
+		brick.doubleThick = true;
+		brick.color = ConsoleColor::DarkGreen;
+		bricks.push_back(brick);
+	}
+		
 }
 
 void Game::ResetBall()
@@ -69,7 +75,9 @@ void Game::Render() const
 	ball.Draw();
 
 	// TODO #3 - Update render to render all bricks
-	brick.Draw();
+	for (const Box& brick : bricks) {
+		brick.Draw();
+	}
 
 	Console::Lock(false);
 }
